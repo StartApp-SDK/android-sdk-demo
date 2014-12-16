98dc97b169a17c11885f5ab43a60e382ed59e7e1
StartApp-SDK <sharon.shauli@startapp.com>
2014-12-16 17:51:27 +0200

    Minor change

--- gen/com/startapp/startappinterstitialexample/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.startapp.startappinterstitialexample;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
